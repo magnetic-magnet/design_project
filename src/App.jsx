6ce92c2af46458680311ccf60edcacc5cce0a348
magnetic-magnet <nirdesh.lama@aism-moz.com>
2024-03-11 10:05:26 +0200

    remove comments

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
     return (
     <>
         {/* <Home />
-        <ToDoList />*/}
-        <PomodoroTimer />
+        <ToDoList />
+        <PomodoroTimer />*/}
         <NavBar />
     </>
     );
